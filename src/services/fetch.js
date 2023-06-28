377f7e52ea56a3eea45f97d9fafe372fd116d48c
mateopautassoPC <mateopautasso.mg@gmail.com>
2023-06-27 23:34:00 -0300

    Correción en services fetch

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -1,11 +1,17 @@
 const fetchGetReq = async (url) => {
-    try {
-        const request = await fetch(url);
-        if(!request.ok) throw ([request.status, request. statusText]);
-        const response = await request.json();
-        return response;
-    } catch(err) {
-        if(err[0] !== undefined) console.log(`Error en la petición: ${err[0]} ${err[1]}`)
-    }
+    // try {
+    //     const request = await fetch(url);
+    //     if(!request.ok) throw ([request.status, request. statusText]);
+    //     const response = await request.json();
+    //     return response;
+    // } catch(err) {
+    //     if(err[0] !== undefined) console.log(`Error en la petición: ${err[0]} ${err[1]}`)
+    // }
+
+    // Adaptación para que no me retorne un throw
+    const request = await fetch(url);
+    if(!request.ok) throw ([request.status, request. statusText]);
+    const response = await request.json();
+    return response;
 }
 export default fetchGetReq;
